ece3dab0fda07307d3a5fa77bf5b6854859f3bb5
erikwtn <erik.wtn@gmail.com>
2023-01-31 15:03:14 +0000

    tooth tracker + scene switcher

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public GameObject Lemon, Bonsai, Orchid, Rose, Tomato;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("ClickSprite"))
+                {
+                    if (hit.collider.gameObject == Lemon)
+                    {
+                        SceneManager.LoadScene(6);
+                    }
+
+                    else if (hit.collider.gameObject == Bonsai)
+                    {
+                        SceneManager.LoadScene(5);
+                    }
+
+                    else if (hit.collider.gameObject == Orchid)
+                    {
+                        SceneManager.LoadScene(7);
+                    }
+
+                    else if (hit.collider.gameObject == Rose)
+                    {
+                        SceneManager.LoadScene(8);
+                    }
+
+                    else if (hit.collider.gameObject == Tomato)
+                    {
+                        SceneManager.LoadScene(9);
+                    }
+                }
+            }
+        }
+    }
+}
+
